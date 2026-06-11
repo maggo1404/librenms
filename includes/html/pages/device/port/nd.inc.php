6bdeea3086e768dec7a95ac1050a0a640565c8df
Marco <509301+maggo1404@users.noreply.github.com>
2026-06-11 14:49:10 +0200

    Correct MAC address header capitalization

--- includes/html/pages/device/port/nd.inc.php
+++ includes/html/pages/device/port/nd.inc.php
@@ -7,7 +7,7 @@ $no_refresh = true;
 <table id="port-nd" class="table table-condensed table-hover table-striped">
     <thead>
         <tr>
-            <th data-column-id="mac_address" data-formatter="tooltip">MAC address</th>
+            <th data-column-id="mac_address" data-formatter="tooltip">MAC Address</th>
             <th data-column-id="mac_oui" data-sortable="false" data-width="" data-visible="<?php echo \App\Facades\LibrenmsConfig::get('mac_oui.enabled') ? 'true' : 'false' ?>" data-formatter="tooltip">Vendor</th>
             <th data-column-id="ipv6_address" data-formatter="tooltip">IPv6 address</th>
             <th data-column-id="remote_device" data-sortable="false">Remote device</th>
